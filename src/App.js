bef968bae1badd676786d84ddaba2dd8ca5e306c
SaiKumarMajji <105416584+SaiKumarMajji@users.noreply.github.com>
2023-07-23 22:17:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import LocationSelector from './LocationSelector';
+import FlightList from './FlightList';
 
-function App() {
+const App = () => {
+  const [flights, setFlights] = useState([]);
+
+  const handleSearch = (pointA, pointB, date) => {
+    // Make the API call to fetch flights from the backend server
+    axios.post('http://localhost:6000/flights', {
+      from: pointA,
+      to: pointB,
+      date: date
+    })
+      .then((response) => {
+        setFlights(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching flights:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Airport Transfer Booking App</h1>
+      <LocationSelector onSearch={handleSearch} />
+      <FlightList flights={flights} />
     </div>
   );
-}
+};
 
 export default App;
+
