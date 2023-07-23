5fb22e31a6dddc0946bf23dcbe042cf6a5c4cce2
SaiKumarMajji <105416584+SaiKumarMajji@users.noreply.github.com>
2023-07-23 23:31:17 +0530

    Update FlightList.jsx

--- src/FlightList.jsx
+++ src/FlightList.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
+
 const FlightList = ({ flights }) => {
-  return (
-    <div>
-      <h2>Available Flights:</h2>
-      {flights.length > 0 ? (
-        <ul>
-          {flights.map((flight) => (
-            <li key={flight.id}>
-              Flight Number: {flight.flightNumber}, Destination: {flight.to}, Departure Time: {flight.departureTime}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No flights available for this travel.</p>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className="flight-list">
+        {flights.length > 0 && (
+          <>
+            <h2>Available Flights:</h2>
+            <ul>
+              {flights.map((flight) => (
+                <li key={flight.id}>
+                  Flight Number: {flight.flightNumber}, Destination: {flight.to}, Departure Time: {flight.departureTime}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    );
+  };
 
 export default FlightList;
